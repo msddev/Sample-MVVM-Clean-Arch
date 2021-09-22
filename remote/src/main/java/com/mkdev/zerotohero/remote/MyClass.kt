c9d73e9dc210c9794d621e68182921e84f29d5b2
mkdev <msd.khoshkam@gmail.com>
2021-09-22 14:02:04 +0330

    Added cache and remote layer

---
+++ remote/src/main/java/com/mkdev/zerotohero/remote/MyClass.kt
@@ -0,0 +1,4 @@
+package com.mkdev.zerotohero.remote
+
+class MyClass {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,6 @@ rootProject.name = "Zero To Hero"
 include(":app")
 include(":data")
 include(":domain")
+include(":presentation")
+include(":cache")
+include(":remote")
